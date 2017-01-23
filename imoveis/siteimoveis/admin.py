53b17a27d017cc77bfe9685da55f1102fe18780c
james peres <james.peres.s@gmail.com>
2017-01-23 15:38:00 -0200

    update admin

--- imoveis/siteimoveis/admin.py
+++ imoveis/siteimoveis/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from .models import Vendedor, Imovel
 
-#class VendedorAdmin(admin.ModelAdmin):
-#    pass
-
-
-#class ImovelAdmin(admin.ModelAdmin):
-#    pass
-
 
 class ImovelAdmin(admin.ModelAdmin):
     readonly_fields = ('image_tag','endereco', 'valor', 'bairro', 'anuncio', 'vendedor', 'cidade', 'latitude', 'longitude')
     list_display = ('image_tag', 'endereco', 'valor', 'bairro', 'anuncio',  'vendedor')
-    #def __str__(self):
-    #    return self.anuncio
 
 admin.site.register(Imovel, ImovelAdmin)
